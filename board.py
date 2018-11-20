8470ef099f15146a4fa91d8f6e93284dd21c7824
Kai Zhao <kaizhao02@berkeley.edu>
2018-11-19 19:55:22 -0500

    Added a block class

--- board.py
+++ board.py
@@ -3,7 +3,7 @@ from pieces import Piece
 
 class Board:
 
-	def __init__(self, width = 8, height = 20):
+	def __init__(self, width = 10, height = 20):
 		self.width = width
 		self.height = height
 		self.board = [['.'] * width for _ in range(height)]
@@ -20,14 +20,14 @@ class Board:
 			self.curr_piece.move_down()
 
 		self.curr_piece.move_up()
-		self.update(self.curr_piece)
+		self.set_piece(self.curr_piece)
 		self.curr_piece = self.get_random_piece()
 
 	def move_curr_down(self):
 		self.curr_piece.move_down()
 		if not self.can_place_piece(self.curr_piece):
 			self.curr_piece.move_up()
-			self.update(self.curr_piece)
+			self.set_piece(self.curr_piece)
 			self.curr_piece = self.get_random_piece()
 
 	def move_curr_left(self):
@@ -48,16 +48,24 @@ class Board:
 		self.clear_bottom()
 
 	def can_place_piece(self, piece):
-		for point in piece.points:
-			x = point[0]
-			y = point[1]
-			bottom = False
-			if (x >= self.height or y < 0 
-								 or y >= self.width 
-								 or self.board[x][y] != '.'):
+		for block in piece.blocks:
+			if (block.row >= self.height or block.col < 0
+										 or block.col >= self.width
+										 or self.board[block.row][block.col] != "."): 
 				return False
 
 		return True
+
+		# for point in piece.points:
+		# 	x = point[0]
+		# 	y = point[1]
+		# 	bottom = False
+		# 	if (x >= self.height or y < 0 
+		# 						 or y >= self.width 
+		# 						 or self.board[x][y] != '.'):
+		# 		return False
+
+		# return True
 
 	def clear_bottom(self):
 		for i in range(self.height - 1, 0, -1):
@@ -65,19 +73,28 @@ class Board:
 				self.board[i][j] = self.board[i - 1][j]
 
 	def check_bottom(self, piece):
-		for point in piece.points:
-			x = point[0] + 1
-			y = point[1]
-			bottom = False
-			if x >= self.height or self.board[x][y] != '.':
-				self.update_board(piece)
+		for block in piece.blocks:
+			if block.row >= self.height or self.board[x][y] != ".":
+				self.set_piece(piece)
 				return True
 
-		return False 
+		return False
 
-	def update(self, piece):
-		for point in piece.points:
-				self.board[point[0]][point[1]] = '@'
+		# for point in piece.points:
+		# 	x = point[0] + 1
+		# 	y = point[1]
+		# 	bottom = False
+		# 	if x >= self.height or self.board[x][y] != '.':
+		# 		self.update_board(piece)
+		# 		return True
+
+		# return False 
+
+	def set_piece(self, piece):
+		for block in piece.blocks:
+			self.board[block.row][block.col] = "@"
+		# for point in piece.points:
+		# 		self.board[point[0]][point[1]] = '@'
 
 	def print_board(self):
 		temp = [row[:] for row in self.board]
@@ -87,12 +104,19 @@ class Board:
 			for j in range(self.width):
 				found = False
 				
-				for point in self.curr_piece.points:
-					if point[0] == i and point[1] == j:
+				for block in self.curr_piece.blocks:
+					if block.row == i and block.col == j:
 						row += "@ "
 						found = True
 				if not found:
-					row += temp[i][j] + ' '
+					row += temp[i][j] + " "
+
+				# for point in self.curr_piece.points:
+				# 	if point[0] == i and point[1] == j:
+				# 		row += "@ "
+				# 		found = True
+				# if not found:
+				# 	row += temp[i][j] + ' '
 
 
 				
--- pieces.py
+++ pieces.py
@@ -1,34 +1,52 @@
 
+class Block:
+
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+
 class Piece:
 
 	def __init__(self, piece_type):
-		self.type = piece_type 
+		self.type = piece_type
+		self.blocks = [] 
 		self.init_points()
 
 	def init_points(self):
 		if self.type == "I":
-			self.points = [(0, 3), (1, 3), (2, 3), (3, 3)]
+			self.blocks.extend([Block(0, 3), Block(1, 3), Block(2, 3), Block(3, 3)])
+			# self.points = [(0, 3), (1, 3), (2, 3), (3, 3)]
 		elif self.type == "T":
-			self.points = [(0, 2), (0, 3), (0, 4), (1, 3)]
+			self.blocks.extend([Block(0, 2), Block(0, 3), Block(0, 4), Block(1, 3)])
+			# self.points = [(0, 2), (0, 3), (0, 4), (1, 3)]
 
 	def move_down(self):
-		for i in range(len(self.points)):
-			self.points[i] = (self.points[i][0] + 1, self.points[i][1])
+		for block in self.blocks:
+			block.row += 1
+		# for i in range(len(self.points)):
+		# 	self.points[i] = (self.points[i][0] + 1, self.points[i][1])
 
 	def move_up(self):
-		for i in range(len(self.points)):
-			self.points[i] = (self.points[i][0] - 1, self.points[i][1])
+		for block in self.blocks:
+			block.row -= 1
+		# for i in range(len(self.points)):
+		# 	self.points[i] = (self.points[i][0] - 1, self.points[i][1])
 
 	def move_left(self):
-		for i in range(len(self.points)):
-			self.points[i] = (self.points[i][0], self.points[i][1] - 1)
+		for block in self.blocks:
+			block.col -= 1
+		# for i in range(len(self.points)):
+		# 	self.points[i] = (self.points[i][0], self.points[i][1] - 1)
 
 	def move_right(self):
-		for i in range(len(self.points)):
-			self.points[i] = (self.points[i][0], self.points[i][1] + 1)
+		for block in self.blocks:
+			block.col += 1		
 
-	def rotate(self):
-		for i in range(len(self.points)):
-			self.points[i] = (self.points[i][0] + 1, self.points[i][1])
+		# for i in range(len(self.points)):
+		# 	self.points[i] = (self.points[i][0], self.points[i][1] + 1)
+
+	# def rotate(self):
+	# 	for i in range(len(self.points)):
+	# 		self.points[i] = (self.points[i][0] + 1, self.points[i][1])
 
 	# def move_to(self, row, col):
